d83fe7c03221874c701c1a63d770b31a901708df
Christian Ølholm <clholm@ebay.com>
2016-03-15 22:02:24 +0100

    Got test project up and running

--- OptimizelyNet.Tests/UnitTest1.cs
+++ OptimizelyNet.Tests/UnitTest1.cs
@@ -1,14 +1,26 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OptimizelyNet;
 
 namespace OptimizelyNet.Tests
 {
     [TestClass]
     public class UnitTest1
     {
+        private readonly string ApiToken;
+        public UnitTest1()
+        {
+            ApiToken = ConfigurationManager.AppSettings.Get("ApiToken");
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            var optimizely = new OptimizelyNet(ApiToken);
+            var projects = optimizely.GetProjects();
+
+            Assert.IsNotNull(projects);
         }
     }
 }
--- OptimizelyNet/DTO/Goal.cs
+++ OptimizelyNet/DTO/Goal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using RestSharp.Deserializers;
 
 // ReSharper disable InconsistentNaming
 
