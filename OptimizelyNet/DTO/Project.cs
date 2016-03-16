aceaca3ebf635a76f9da511571270b8eb50e7b35
Christian Ølholm <christian@codepoet.dk>
2016-03-16 20:41:30 +0100

    Paste JSON as class is awesome :)

--- OptimizelyNet/DTO/Project.cs
+++ OptimizelyNet/DTO/Project.cs
@@ -15,14 +15,14 @@ namespace OptimizelyNet.DTO
         public DateTime last_modified { get; set; }
         public string library { get; set; }
         public bool include_jquery { get; set; }
-        public string js_file_size { get; set; }
+        public ulong js_file_size { get; set; }
         public string project_javascript { get; set; }
         public bool enable_force_variation { get; set; }
         public bool exclude_disabled_experiments { get; set; }
         public bool exclude_names { get; set; }
         public bool ip_anonymization { get; set; }
         public string ip_filter { get; set; }
-        public string socket_tocken { get; set; }
+        public string socket_token { get; set; }
         public string dcp_service_id { get; set; }
     }
 }
